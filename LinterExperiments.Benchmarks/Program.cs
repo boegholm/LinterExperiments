33b0353716a98ad2c9baa03b343873c2d87721d0
Thomas Bøgholm <thomasboegholm@gmail.com>
2022-06-27 08:53:05 +0200

    Add project files.

---
+++ LinterExperiments.Benchmarks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using LinterExperiments.Smells.CodeSmellExamples;
+
+namespace LinterExperiments.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<LinterExperiments>();
+        }
+    }
+    [RPlotExporter]
+    public class LinterExperiments
+    {
+        private static DeadLocalStoreBase bad = new DeadLocalStoreBad();
+        private static DeadLocalStoreBase good = new DeadLocalStoreGood();
+        //private SHA256 sha256 = SHA256.Create();
+        //private MD5 md5 = MD5.Create();
+        //private byte[] data;
+
+        //[Params(1000, 10000)]
+        //public int N;
+
+        //[GlobalSetup]
+        //public void Setup()
+        //{
+        //    data = new byte[N];
+        //    new Random(42).NextBytes(data);
+        //}
+        int n = 5_000_000;
+        [Benchmark]
+        public void Good()
+        {
+            for(int i=0;i<n;i++)
+                good.DeadLocalStore(0.4250);
+        }
+
+        [Benchmark]
+        public void Bad()
+        {
+            for (int i = 0; i < n; i++)
+                bad.DeadLocalStore(0.4250);
+        }
+    }
+}
---
+++ LinterExperiments/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace LinterExperiments
+{
+
+    
+    namespace Smells.CodeSmellExamples
+    {
+        public abstract class DeadLocalStoreBase
+        {
+            public abstract double DeadLocalStore(double radius);
+        }
+
+        public class DeadLocalStoreGood : DeadLocalStoreBase
+        {
+            public override double DeadLocalStore(double radius)
+            {
+                double pi = 3.14;
+
+                return (pi * Math.Pow(2, radius));
+            }
+        }
+
+        public class DeadLocalStoreBad : DeadLocalStoreBase
+        {
+            public override double DeadLocalStore(double radius)
+            {
+                double pi = 3.14;
+                double notPi = 4.13;
+                int cousinsAgeInMonths = 146;
+                string niceGreeting = "Hellow World";
+
+                return (pi * Math.Pow(2, radius));
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

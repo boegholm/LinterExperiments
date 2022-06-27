a9c4ddac70953fafc3ca2efd862859fa3d7cf10c
boegholm <github.com@glirc.dk>
2022-06-27 12:43:20 +0200

    selectable

--- LinterExperiments.Benchmarks/Program.cs
+++ LinterExperiments.Benchmarks/Program.cs
@@ -64,43 +64,44 @@ namespace LinterExperiments.Benchmarks
     {
         public static void Main(string[] args)
         {
+            var summary = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args: args);
             //var summary = BenchmarkRunner.Run<AllLinterEx>( args: args);
-            var summary = BenchmarkRunner.Run<LinterTypeCheck>( args: args);
+            //var summary = BenchmarkRunner.Run<LinterTypeCheck>( args: args);
 
             //var summary = BenchmarkRunner.Run<LinterExperiments>(args:args);
             //var summary = BenchmarkRunner.Run(typeof(LinterProgram).Assembly, args: args);
             //var othersum = BenchmarkSwitcher.FromAssembly(typeof(LinterProgram).Assembly).Run(args: args);
         }
     }
-    [RPlotExporter]
-    public class LinterExperiments
-    {
-        private DeadLocalStoreBase bad = new DeadLocalStoreBad();
-        private DeadLocalStoreBase good = new DeadLocalStoreGood();
-        //private SHA256 sha256 = SHA256.Create();
-        //private MD5 md5 = MD5.Create();
-        //private byte[] data;
+    //[RPlotExporter]
+    //public class LinterExperiments
+    //{
+    //    private DeadLocalStoreBase bad = new DeadLocalStoreBad();
+    //    private DeadLocalStoreBase good = new DeadLocalStoreGood();
+    //    //private SHA256 sha256 = SHA256.Create();
+    //    //private MD5 md5 = MD5.Create();
+    //    //private byte[] data;
 
-        //[Params(1000, 10000)]
-        //public int N;
+    //    //[Params(1000, 10000)]
+    //    //public int N;
 
-        //[GlobalSetup]
-        //public void Setup()
-        //{
-        //    data = new byte[N];
-        //    new Random(42).NextBytes(data);
-        //}
-        int n = 5_000_000;
-        [Benchmark]
-        public double Good()
-        {
-            return good.DeadLocalStore(0.4250);
-        }
+    //    //[GlobalSetup]
+    //    //public void Setup()
+    //    //{
+    //    //    data = new byte[N];
+    //    //    new Random(42).NextBytes(data);
+    //    //}
+    //    int n = 5_000_000;
+    //    [Benchmark]
+    //    public double Good()
+    //    {
+    //        return good.DeadLocalStore(0.4250);
+    //    }
 
-        [Benchmark]
-        public double Bad()
-        {
-            return bad.DeadLocalStore(0.4250);
-        }
-    }
+    //    [Benchmark]
+    //    public double Bad()
+    //    {
+    //        return bad.DeadLocalStore(0.4250);
+    //    }
+    //}
 }
